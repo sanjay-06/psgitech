b6460b5925a4841a3953bd23bc2b0d361d6cff01
Sanjay T <59244273+sanjay-06@users.noreply.github.com>
2021-05-31 11:47:53 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,86 +9,85 @@ jQuery(document).ready(function($) {
    "use strict";
 
    
-
    var siteMenuClone = function() {
 
-       $('.js-clone-nav').each(function() {
-           var $this = $(this);
-           $this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
-       });
+    $('.js-clone-nav').each(function() {
+        var $this = $(this);
+        $this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+    });
 
 
-       setTimeout(function() {
-           
-           var counter = 0;
-     $('.site-mobile-menu .has-children').each(function(){
-       var $this = $(this);
-       
-       $this.prepend('<span class="arrow-collapse collapsed">');
+    setTimeout(function() {
+        
+        var counter = 0;
+  $('.site-mobile-menu .has-children').each(function(){
+    var $this = $(this);
+    
+    $this.prepend('<span class="arrow-collapse collapsed">');
 
-       $this.find('.arrow-collapse').attr({
-         'data-toggle' : 'collapse',
-         'data-target' : '#collapseItem' + counter,
-       });
+    $this.find('.arrow-collapse').attr({
+      'data-toggle' : 'collapse',
+      'data-target' : '#collapseItem' + counter,
+    });
 
-       $this.find('> ul').attr({
-         'class' : 'collapse',
-         'id' : 'collapseItem' + counter,
-       });
+    $this.find('> ul').attr({
+      'class' : 'collapse',
+      'id' : 'collapseItem' + counter,
+    });
 
-       counter++;
+    counter++;
 
-     });
+  });
 
-   }, 1000);
+}, 1000);
 
-       $('body').on('click', '.arrow-collapse', function(e) {
-     var $this = $(this);
-     if ( $this.closest('li').find('.collapse').hasClass('show') ) {
-       $this.removeClass('active');
-     } else {
-       $this.addClass('active');
-     }
-     e.preventDefault();  
-     
-   });
+    $('body').on('click', '.arrow-collapse', function(e) {
+  var $this = $(this);
+  if ( $this.closest('li').find('.collapse').hasClass('show') ) {
+    $this.removeClass('active');
+  } else {
+    $this.addClass('active');
+  }
+  e.preventDefault();  
+  
+});
 
-       $(window).resize(function() {
-           var $this = $(this),
-               w = $this.width();
+    $(window).resize(function() {
+        var $this = $(this),
+            w = $this.width();
 
-           if ( w > 768 ) {
-               if ( $('body').hasClass('offcanvas-menu') ) {
-                   $('body').removeClass('offcanvas-menu');
-               }
-           }
-       })
+        if ( w > 768 ) {
+            if ( $('body').hasClass('offcanvas-menu') ) {
+                $('body').removeClass('offcanvas-menu');
+            }
+        }
+    })
 
-       $('body').on('click', '.js-menu-toggle', function(e) {
-           var $this = $(this);
-           e.preventDefault();
+    $('body').on('click', '.js-menu-toggle', function(e) {
+        var $this = $(this);
+        e.preventDefault();
 
-           if ( $('body').hasClass('offcanvas-menu') ) {
-               $('body').removeClass('offcanvas-menu');
-               $this.removeClass('active');
-           } else {
-               $('body').addClass('offcanvas-menu');
-               $this.addClass('active');
-           }
-       }) 
+        if ( $('body').hasClass('offcanvas-menu') ) {
+            $('body').removeClass('offcanvas-menu');
+            $this.removeClass('active');
+        } else {
+            $('body').addClass('offcanvas-menu');
+            $this.addClass('active');
+        }
+    }) 
 
-       // click outisde offcanvas
-       $(document).mouseup(function(e) {
-       var container = $(".site-mobile-menu");
-       if (!container.is(e.target) && container.has(e.target).length === 0) {
-         if ( $('body').hasClass('offcanvas-menu') ) {
-                   $('body').removeClass('offcanvas-menu');
-               }
-       }
-       });
-   }; 
-   siteMenuClone();
+    // click outisde offcanvas
+    $(document).mouseup(function(e) {
+    var container = $(".site-mobile-menu");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ( $('body').hasClass('offcanvas-menu') ) {
+                $('body').removeClass('offcanvas-menu');
+            }
+    }
+    });
+}; 
 
+siteMenuClone();
 
    var sitePlusMinus = function() {
        $('.js-btn-minus').on('click', function(e){
@@ -122,6 +121,17 @@ jQuery(document).ready(function($) {
    };
    // siteSliderRange();
 
+   $(window).scroll(function() {
+
+    if ($(this).scrollTop()>0)
+     {
+        $('#fade').fadeOut();
+     }
+    else
+     {
+      $('#fade').fadeIn();
+     }
+ });
 
    var siteMagnificPopup = function() {
        $('.image-popup').magnificPopup({
@@ -168,7 +178,7 @@ jQuery(document).ready(function($) {
            margin: 20,
            nav: true,
            dots: true,
-               navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+           navText: ['<span class="fa fa-angle-double-left" aria-hidden="true">', '<span class="fa fa-angle-double-right">'],
            responsive:{
            600:{
                margin: 20,
@@ -201,8 +211,8 @@ jQuery(document).ready(function($) {
            margin: 20,
            nav: true,
            dots: true,
-               navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
-           responsive:{
+           navText: ['<span class="fa fa-angle-double-left" aria-hidden="true">', '<span class="fa fa-angle-double-right">'],
+          responsive:{
            600:{
                margin: 20,
                stagePadding: 0,
@@ -228,7 +238,7 @@ jQuery(document).ready(function($) {
            margin: 20,
            nav: true,
            dots: true,
-               navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+           navText: ['<span class="fa fa-angle-double-left" aria-hidden="true">', '<span class="fa fa-angle-double-right">'],
            responsive:{
            600:{
                margin: 20,
@@ -267,7 +277,7 @@ jQuery(document).ready(function($) {
            animateOut: 'fadeOut',
            animateIn: 'fadeIn',
            nav: true,
-           navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">']
+           navText: ['<span class="fa fa-angle-double-left" aria-hidden="true">', '<span class="fa fa-angle-double-right">']
          });
      }
    };
